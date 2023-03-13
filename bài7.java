9d91276af55aee1bcd44920dda891ab41b4b7b86
NguyenThiPhuongTham <125626402+NguyenThiPhuongTham@users.noreply.github.com>
2023-03-13 19:28:49 +0700

    Add files via upload

---
+++ bài7.java
@@ -0,0 +1,9 @@
+package lab2;
+
+public class bài7 {
+	 public static void main(String[] args) {
+	        for(int i = 1; i <= 20; i++) {
+	            System.out.print(i + " ");
+	        }
+	    }
+}
